5113cd147447cce4ca3ffbac7f566882aa8f6206
bakemonostan <ehizomokhomion@gmail.com>
2022-11-06 14:55:44 -0800

    image component

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="image">
+    <img v-for="post in posts" :key="post.id" :src="post.image" alt="" />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['posts']);
+</script>
+
+<style scoped>
+.image {
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
+}
+
+img {
+  margin: 5px;
+  width: 200px;
+}
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -9,19 +9,29 @@
           following: 69,
         }"
       />
+      <ImageGallery
+        :posts="[
+          {
+            id: 1,
+            image:
+              'https://akns-images.eonline.com/eol_images/Entire_Site/202288/rs_1200x1200-220908155209-1200.selena-gomez-my-mind-me-doc.jpg?fit=around%7C1080:1080&output-quality=90&crop=1080:1080;center,top',
+          },
+        ]"
+      />
     </div>
   </Container>
 </template>
 
 <script setup>
 import Container from './Container.vue';
+import ImageGallery from './ImageGallery.vue';
 import UserBar from './UserBar.vue';
 </script>
 
 <style scoped>
 .profile {
   display: flex;
-  align-items: center;
+  flex-direction: column;
   padding: 20px 0;
 }
 </style>
